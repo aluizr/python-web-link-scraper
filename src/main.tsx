cda7d0f85551df2e0b70f2068e3201d127225d8c
AndreRibeiro <aluizrr@yahoo.com.br>
2026-03-07 22:40:12 -0300

    feat: add new color themes and background gradients for improved UI aesthetics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "../public/output.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
